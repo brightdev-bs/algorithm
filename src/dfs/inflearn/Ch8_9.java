e490d4ce112881418faa967ac51e6f4c89d5e40e
Byeongsu KIM <brightdev.bs@gmail.com>
2023-01-28 19:55:42 +0900

    DFS

---
+++ src/dfs/inflearn/Ch8_9.java
@@ -0,0 +1,35 @@
+package dfs.inflearn;
+
+import java.util.Scanner;
+
+// 조합 수 구하기
+public class Ch8_9 {
+
+    static int n, m;
+    static int[] ch, out;
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        n = sc.nextInt();
+        m = sc.nextInt();
+        ch = new int[n + 1];
+        out = new int[m];
+
+        DFS(0, 1);
+    }
+
+    public static void DFS(int L, int s) {
+        if(L == m) {
+            for (int i : out) {
+                System.out.print(i + " ");
+            }
+            System.out.println();
+        } else {
+            for(int i = s; i <= n; i++) {
+                out[L] = i;
+                DFS(L + 1, i + 1);
+            }
+        }
+    }
+}
