1e80944c228d191745e60b71ccdca78c7333f276
Byeongsu KIM <brightdev.bs@gmail.com>
2023-01-30 23:30:08 +0900

    다익스트라 알고리즘

---
+++ src/dijkstra/inflearn/Ch9_5.java
@@ -0,0 +1,60 @@
+package dijkstra.inflearn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Ch9_5 {
+
+    static ArrayList<ArrayList<Edge>> graph;
+    static int[] dis;
+    static class Edge implements Comparable<Edge>{
+        public int vex;
+        public int cost;
+        Edge(int vex, int cost) {
+            this.vex = vex;
+            this.cost = cost;
+        }
+        @Override
+        public int compareTo(Edge ob){
+            return this.cost-ob.cost;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        graph = new ArrayList<>();
+        for(int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        dis = new int[n + 1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        for(int i = 0; i < m; i++) {
+            int v1 = sc.nextInt();
+            int v2 = sc.nextInt();
+            int c = sc.nextInt();
+            graph.get(v1).add(new Edge(v2, c));
+        }
+
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.offer(new Edge(1, 0));
+        dis[1] = 0;
+        while(!pq.isEmpty()) {
+            Edge tmp = pq.poll();
+            int now = tmp.vex;
+            int nowCost = tmp.cost;
+            if(nowCost > dis[now]) continue;
+            for (Edge edge : graph.get(now)) {
+                if(dis[edge.vex] > nowCost + edge.cost) {
+                    dis[edge.vex] = nowCost + edge.cost;
+                    pq.offer(new Edge(edge.vex, nowCost + edge.cost));
+                }
+            }
+        }
+    }
+}
---
+++ src/dijkstra/inflearn/Ch9_5_R.java
@@ -0,0 +1,63 @@
+package dijkstra.inflearn;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Scanner;
+
+public class Ch9_5_R {
+
+    static class Edge implements Comparable<Edge> {
+        int e, v;
+
+        public Edge(int e, int v) {
+            this.e = e;
+            this.v = v;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.v - o.v;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int m = sc.nextInt();
+
+        ArrayList<ArrayList<Edge>> list = new ArrayList<>();
+        for(int i = 0; i <= n; i++) {
+            list.add(new ArrayList<>());
+        }
+
+        for(int i = 0; i < m; i++) {
+            int a = sc.nextInt();
+            int b = sc.nextInt();
+            int c = sc.nextInt();
+            list.get(a).add(new Edge(b, c));
+        }
+
+        int[] dis = new int[n + 1];
+        Arrays.fill(dis, Integer.MAX_VALUE);
+        PriorityQueue<Edge> pq = new PriorityQueue<>();
+        pq.add(new Edge(1, 0));
+        dis[1] = 0;
+        while(!pq.isEmpty()) {
+            Edge tmp = pq.poll();
+            int now = tmp.e;
+            int nowCost = tmp.v;
+            if(dis[now] < nowCost) continue;
+            for (Edge edge : list.get(now)) {
+                if(dis[edge.e] > edge.v + dis[now]) dis[edge.e] = edge.v + dis[now];
+                pq.add(edge);
+            }
+        }
+
+        for (int di : dis) {
+            System.out.println(di);
+        }
+
+    }
+}
