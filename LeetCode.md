6b790bb842f7bd5cc10fc4e32091fc8679d2961f
Byeongsu KIM <brightdev.bs@gmail.com>
2023-05-06 22:20:01 +0200

    알고리즘 공부 - leetcode75

--- LeetCode.md
+++ LeetCode.md
@@ -6,3 +6,6 @@
 ### Array
 1. [Two Sum](/src/leetcode/array/TwoSum.java) (easy)
 2. [Best Time to Buy and Sell Stock](/src/leetcode/array/BestTimeToBuyAndSellStock.java) (mid) *카테인 알고리즘
+3. [Contains Duplicate](/src/leetcode/array/ContainsDuplicate.java)
+4. [Product of Array Except Self](/src/leetcode/array/ProductOfArrayExceptSelf.java)
+
---
+++ src/leetcode/array/ProductOfArrayExceptSelf.java
@@ -0,0 +1,19 @@
+package leetcode.array;
+
+public class ProductOfArrayExceptSelf {
+    class Solution {
+        public int[] productExceptSelf(int[] nums) {
+            int[] answer = new int[nums.length];
+            for(int i = 0, tmp = 1; i < nums.length; i++) {
+                answer[i] = tmp;
+                tmp *= nums[i];
+            }
+
+            for(int i = nums.length - 1, tmp = 1; i >= 0; i--) {
+                answer[i] *= tmp;
+                tmp *= nums[i];
+            }
+            return answer;
+        }
+    }
+}
