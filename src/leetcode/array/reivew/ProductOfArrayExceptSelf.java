6258cfc20d3183aac8d47b618986c1ec8cfc1512
Byeongsu KIM <brightdev.bs@gmail.com>
2023-06-21 09:01:04 +0900

    알고리즘 공부 - Array 복습

---
+++ src/leetcode/array/reivew/ProductOfArrayExceptSelf.java
@@ -0,0 +1,19 @@
+package leetcode.array.reivew;
+
+public class ProductOfArrayExceptSelf {
+
+    public int[] productExceptSelf(int[] nums) {
+        int n = nums.length;
+        int[] answer = new int[n];
+        for(int i = 0, tmp = 1; i < n; i++) {
+            answer[i] = tmp;
+            tmp *= nums[i];
+        }
+
+        for(int i = n - 1, tmp = 1; i >= 0; i--) {
+            answer[i] *= tmp;
+            tmp *= nums[i];
+        }
+        return answer;
+    }
+}
